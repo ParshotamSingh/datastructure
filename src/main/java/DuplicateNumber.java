1f802b976138b7779c0d88f2859066150aa4d85c
ParshotamSingh <parshotam_singh@outlook.com>
2024-05-18 16:28:27 +0530

    DuplicateNumber

---
+++ src/main/java/DuplicateNumber.java
@@ -0,0 +1,20 @@
+package main.java;
+
+class DuplicateNumber {
+    public static void main(String[] args) {
+        int arr[] = {1,5,5,3,4,2};
+        System.out.println(findDuplicate(arr));
+    }
+    public static int findDuplicate(int[] nums) {
+
+        for(int i = 0; i<nums.length-1;i++){
+            int num = nums[i];
+            for(int j = i+1; j<nums.length;j++){
+                if(nums[j]==num){
+                    return num;
+                }
+            }
+        }
+        return 0;
+    }
+}
