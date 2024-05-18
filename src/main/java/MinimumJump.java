24b03af5c0a6cee8f1e6ca3e40aeda80b8c5015a
ParshotamSingh <parshotam_singh@outlook.com>
2024-05-18 15:48:55 +0530

    MinimumJump

---
+++ src/main/java/MinimumJump.java
@@ -0,0 +1,39 @@
+package main.java;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MinimumJump {
+    public static void main(String args[]){
+        int arr1[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
+        int arr2[] = {1, 4, 3, 2, 6, 7};
+        System.out.println(findMinimumJump(arr1));
+        System.out.println(findMinimumJump(arr2));
+    }
+
+    public static int findMinimumJump(int arr[]){
+
+        int arrLength = arr.length;
+        int sum = 0;
+
+        for(int i=0; i<arrLength-1;i++) {
+            sum = arr[i] + sum;
+            if (sum >= arrLength) {
+                return i;
+            }
+        }
+        return 0;
+    }
+    public static int findMinimumJumpWithStream(int arr[]){
+        int sum = 0;
+        int arrLength = arr.length;
+        for(int i : arr){
+            sum = arr[i] + sum;
+            if (sum >= arrLength) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+}
