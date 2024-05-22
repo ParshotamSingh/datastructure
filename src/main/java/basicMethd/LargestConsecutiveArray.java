16aae26dbc286c1d0502d250a801186a5b24404c
ParshotamSingh <parshotam_singh@outlook.com>
2024-05-22 23:57:06 +0530

    CountInversion and StockProfit

--- src/main/java/basicMethd/LargestConsecutiveArray.java
+++ src/main/java/basicMethd/LargestConsecutiveArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class LargestConsecutiveArray {
     public static void main(String[] args) {
-        int[] arr1 = {2,6,1,9,4,5,3};
+        int[] arr1 = {18,2,15,6,12,1,19,91,13,4,5,14,3,16,17};
         int[] arr2 = {-1,-2,-3,-4};
         System.out.println(getLargestSum(arr1));
 //        System.out.println(getLargestSum(arr2));
@@ -13,26 +13,32 @@ public class LargestConsecutiveArray {
 
     public static int getLargestSum(int[] arr){
         int temp = 0;
-        int k = 0;
+        int k = arr.length-2;
+        boolean flag = false;
         int count = 0;
         int largestArray = 0;
-        for(int i=1;i<arr.length;i++){
+        for(int i=1;i<arr.length+1;i++){
             for(int j=0;j<arr.length-i;j++ ){
                 if(arr[j]>arr[j+1]){
                     temp = arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
                 }
-                k=j+1;
             }
-            if(arr[k]==arr[k-1]){
-                count++;
-            }else {
-                count = 0;
-            }
+            if(flag){
+                if(arr[k]+1==arr[k+1]){
+                    count++;
+                }else {
+                    count = 0;
+                }
             if(count>=largestArray){
                 largestArray = count;
+                if(k==0&&arr[k]+1==arr[k+1])
+                    largestArray++;
             }
+            k--;
+            }
+            flag = true;
         }
         return largestArray;
     }
