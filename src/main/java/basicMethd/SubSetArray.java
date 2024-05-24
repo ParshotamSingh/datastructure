68372d5968c05529699f901c99a007561eef1240
ParshotamSingh <parshotam_singh@outlook.com>
2024-05-24 23:21:34 +0530

    SubSetArray

--- src/main/java/basicMethd/SubSetArray.java
+++ src/main/java/basicMethd/SubSetArray.java
@@ -3,8 +3,24 @@ package main.java.basicMethd;
 public class SubSetArray {
     public static void main(String[] args) {
 
+        int a1[] = {11, 7, 1, 13, 21, 3, 7, 3};
+        int a2[] = {11, 3, 7, 1, 7};
+        System.out.println(isSubSetArray(a1,a2));
+
     }
     public static boolean isSubSetArray(int a1[], int a2[]){
-        return false;
+        boolean flag = false;
+        for(int i=0;i<a1.length;i++){
+            flag = false;
+          for(int j=0;j<a2.length;j++){
+              if(a2[j]==a1[i]) {
+                  flag = true;
+              }
+          }
+          if(!flag){
+              return false;
+          }
+        }
+        return flag;
     }
 }
