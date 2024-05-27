8319f32ce7ebc90659cf54e5b20b950098d58f23
ParshotamSingh <parshotam_singh@outlook.com>
2024-05-28 00:02:04 +0530

    SpiralTraverse

---
+++ src/main/java/Template.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class Template {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        System.out.println(get(a));
+    }
+    public static int get(int a[]){
+        return 0;
+    }
+}
---
+++ src/main/java/matrix/SpiralTraverse.java
@@ -0,0 +1,30 @@
+package main.java.matrix;
+
+public class SpiralTraverse {
+    public static void main(String[] args) {
+        int matrix[][] = {{1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15,16}};
+        System.out.println(get(matrix));
+    }
+    public static int[] get(int a[][])
+    {
+        int x = a.length;
+        int y = a[0].length;
+        int pointA[] = {0,0};
+        int pointB[] = {0,x-1};
+        int pointC[] = {x-1,y-1};
+        int pointD[] = {x-1,0};
+        int mSize = x * y;
+        int outputArray[] = new int[mSize];
+        for(int i=0;i<x;i++){
+            for(int j=0;j<y;j++){
+
+            }
+
+        }
+        return null;
+
+    }
+}
