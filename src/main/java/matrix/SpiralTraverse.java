7d231f66d49d4d6daeed0415543672cd5a542b78
ParshotamSingh <parshotam_singh@outlook.com>
2024-05-29 00:11:29 +0530

    code fix

--- src/main/java/matrix/SpiralTraverse.java
+++ src/main/java/matrix/SpiralTraverse.java
@@ -12,15 +12,19 @@ public class SpiralTraverse {
     {
         int x = a.length;
         int y = a[0].length;
-        int pointA[] = {0,0};
-        int pointB[] = {0,x-1};
-        int pointC[] = {x-1,y-1};
-        int pointD[] = {x-1,0};
+//        int pointA[] = {0,0};
+//        int pointB[] = {0,x-1};
+//        int pointC[] = {x-1,y-1};
+//        int pointD[] = {x-1,0};
+        int pointA = 0;
+        int pointB = x-1;
+        int pointC = x-1;
+        int pointD = 0;
         int mSize = x * y;
         int outputArray[] = new int[mSize];
         for(int i=0;i<x;i++){
-            for(int j=0;j<y;j++){
-
+            for(int j=i;j<y;j++){
+                System.out.println();
             }
 
         }
