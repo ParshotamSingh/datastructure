d5ce806b39acb63fbd8a89c799826c7e09db936a
ParshotamSingh <parshotam_singh@outlook.com>
2024-05-30 23:49:58 +0530

    code fix

--- src/main/java/matrix/SpiralTraverse.java
+++ src/main/java/matrix/SpiralTraverse.java
@@ -6,7 +6,7 @@ public class SpiralTraverse {
                 {5, 6, 7, 8},
                 {9, 10, 11, 12},
                 {13, 14, 15,16}};
-        System.out.println(get(matrix));
+      get(matrix);
     }
     public static int[] get(int a[][])
     {
@@ -21,31 +21,35 @@ public class SpiralTraverse {
         int pointC = x;
         int pointD = 0;
         int mSize = x * y;
-        int turn = 0;
+        int turn = 1;
         int outputArray[] = new int[mSize];
         for(int i=0;i<x;i++){
-            turn++;
             if(turn==1){
                 for(int j=pointA;j<pointB;j++){
                 System.out.print(" "+a[pointA][j]);
                 }
+                turn++;
             }
             if(turn==2){
                 for(int j=1;j<pointC;j++){
                     System.out.print(" "+a[j][pointB-1]);
                 }
+                turn++;
             }
             if(turn==3){
                 for(int j=pointC-2;j>pointD;j--){
                     System.out.print(" "+a[pointC-1][j]);
                 }
+                turn++;
             }
             if(turn==4){
                 for(int j=pointB-1;j>=pointD;j--){
                     System.out.print(" "+a[j][pointA]);
                 }
+                pointA++;
                 turn = 1;
             }
+            System.out.println();
         }
         return null;
     }
