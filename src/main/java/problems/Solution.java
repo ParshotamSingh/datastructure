b645cbbc88f4467f288e6411a2303fa65ba53901
ParshotamSingh <parshotam_singh@outlook.com>
2024-06-04 21:52:49 +0530

    SortMatrix

---
+++ src/main/java/problems/Solution.java
@@ -0,0 +1,29 @@
+package main.java.problems;
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        int a = 0;
+        int b = 0;
+        int n = 0;
+        int var1 = 0;
+        int c = a+1*b;
+            for(int k=1;k<=n;k++){
+                var1 = 0;
+                for(int j=1;j<k;j++){
+                  var1 = getSeries(j)*b+var1;
+                }
+                System.out.print(c+var1+" ");
+            }
+    }
+    public static int getSeries(int base){
+        int a=2;
+
+        for(int i=1;i<base;i++){
+            a = a*2;
+        }
+        return a;
+    }
+}
