dec13eebcdc40362951a07813652564e52e30e8e
ParshotamSingh <parshotam_singh@outlook.com>
2024-06-05 22:06:28 +0530

    SortMatrix

---
+++ src/main/java/matrix/IslandMatrix.java
@@ -0,0 +1,13 @@
+package main.java.matrix;
+
+public class IslandMatrix {
+    public static void main(String[] args) {
+        int grid[][] = {
+                {1, 1, 1, 1, 0},
+                {1, 1, 0, 1, 0},
+                {1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0}};
+
+    }
+
+}
