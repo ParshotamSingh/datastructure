a644fd086987f8d82a9d4c07c03d841f27e77cdb
ParshotamSingh <parshotam_singh@outlook.com>
2024-06-07 01:19:15 +0530

    SortMatrix

---
+++ src/main/java/matrix/CommonElements.java
@@ -0,0 +1,25 @@
+package main.java.matrix;
+
+public class CommonElements {
+
+    public static void main(String[] args) {
+        int grid[][] =
+                {{1, 2, 1, 4, 8},
+                {3, 7, 8, 5, 1},
+                {8, 7, 7, 3, 1},
+                {8, 1, 2, 7, 9},};
+        System.out.println(getIsland(grid));
+
+    }
+    public static int getIsland(int a[][]){
+        int count = 0;
+        for(int i=0;i<a.length;i++){
+            for (int j=0;j<a[0].length;j++){
+                if(a[0][j]==)
+
+            }
+        }
+        return count;
+    }
+
+}
