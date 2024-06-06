c041a5f744a6f5deefe5a36d741b36e0781d24e3
ParshotamSingh <parshotam_singh@outlook.com>
2024-06-07 01:04:00 +0530

    SortMatrix

--- src/main/java/matrix/IslandMatrix.java
+++ src/main/java/matrix/IslandMatrix.java
@@ -6,7 +6,78 @@ public class IslandMatrix {
                 {1, 1, 1, 1, 0},
                 {1, 1, 0, 1, 0},
                 {1, 1, 0, 0, 0},
-                {0, 0, 0, 0, 0}};
+                {0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 0}};
+        System.out.println(getIsland(grid));
+
+    }
+    public static int getIsland(int a[][]){
+        int count = 0;
+     for(int i=0;i<a.length;i++){
+         for (int j=0;j<a[0].length;j++){
+             try {
+                 if(a[i][j]==1){
+                     if(checkIslland(i,j,a)){
+                         count++;
+                     }
+                 }
+
+             }catch (ArrayIndexOutOfBoundsException e){
+
+             }
+         }
+     }
+        return count;
+    }
+    private static boolean checkIslland(int i, int j,int[][]  a){
+        boolean right;
+        boolean left;
+        boolean up;
+        boolean down;
+
+        try {
+            if(a[i+1][j]==0||i==a.length-1){
+                down = true;
+            }else {
+                down = false;
+            }
+        }catch (ArrayIndexOutOfBoundsException e){
+            down = true;
+        }
+        try{
+        if(a[i-1][j]==0||i==0){
+            up = true;
+        }else {
+            up = false;
+        }
+        }catch (ArrayIndexOutOfBoundsException e){
+            up = true;
+        }
+        try{
+        if(a[i][j+1]==0||j==a.length-1){
+            right = true;
+        }else {
+            right = false;
+        }
+        }catch (ArrayIndexOutOfBoundsException e){
+            right = true;
+        }
+        try{
+        if(a[i][j-1]==0||j==0){
+            left = true;
+        }else {
+            left = false;
+        }
+        }catch (ArrayIndexOutOfBoundsException e){
+            left = true;
+        }
+
+
+        if(right && left && up && down){
+            return true;
+        }else {
+            return false;
+        }
 
     }
 
