c0c7f657c8e3e4df88c89294ba621ea585e00cd8
Hpo <sabicode@gmail.com>
2024-11-18 10:13:26 -0700

    code added

---
+++ src/main/java/prianka/opps/Calculator.java
@@ -0,0 +1,11 @@
+package main.java.prianka.opps;
+
+public class Calculator {
+    public static void main(String[] args) {
+//        create object
+        Standard standard = new Standard();
+        int a = standard.plus1(12,30);
+        System.out.println(a);
+
+    }
+}
